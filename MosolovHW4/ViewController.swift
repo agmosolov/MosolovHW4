dd2b931f22218ace5fd1c048c4aafa87ccb46020
Aleksander Mosolov <agmosolov@icloud.com>
2023-03-24 21:37:54 +0300

    final

--- MosolovHW4/ViewController.swift
+++ MosolovHW4/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorDisplayView: UIView!
+    
+    @IBOutlet var redColorLabel: UILabel!
+    @IBOutlet var greenColorLabel: UILabel!
+    @IBOutlet var blueColorLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redColorLabel.text = String(format: "%.2f", redSlider.value)
+        greenColorLabel.text = String(format: "%.2f", greenSlider.value)
+        blueColorLabel.text = String(format: "%.2f", blueSlider.value)
+        colorDisplayView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
     }
+    
 
-
+    @IBAction func changeRedColor() {
+        redColorLabel.text = String(format: "%.2f", redSlider.value)
+        changeColorOnDisplay()
+    }
+    
+    @IBAction func changeGreenColor() {
+        greenColorLabel.text = String(format: "%.2f", greenSlider.value)
+        changeColorOnDisplay()
+        
+    }
+    @IBAction func changeBlueColor() {
+        blueColorLabel.text = String(format: "%.2f", blueSlider.value)
+        changeColorOnDisplay()
+    }
+    
+    private func changeColorOnDisplay() {
+        colorDisplayView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+    }
 }
 
